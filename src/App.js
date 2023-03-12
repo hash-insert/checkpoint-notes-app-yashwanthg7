658873d02ba527277ac8868e7b9b1d081c9b8313
yashwanthg7 <yashwanth18183@gmail.com>
2023-03-12 23:46:34 +0530

    CheckPoint 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import './App.css';
+import React from "react";
+import { useState } from 'react';
 import Header from './components/Header';
 import AddNote from './components/AddNote/AddNote';
+//import Note from './components/note/Note.js';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NoteList/NoteList';
+
 
 function App() {
-  let handleAddNote =(note)=>{
-    console.log(note);
+  let [notes, setNotes] = useState([
+
+  ]);
+  let handleAddNote = (note) => {
+    const current = new Date();
+    const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+    let newNote = {
+      id: nanoid(),
+      content: note,
+      date: date
+    }
+    let newNotes = [...notes, newNote];
+    setNotes(newNotes);
   }
-  return(
-      <div>
-        <Header/>
-        <AddNote addNote={handleAddNote}/>
-      </div>
+
+  let handleDeleteNote = (id)=>{
+    let newNotes=[]
+    for( let note of notes)
+    {
+      if(note.id !== id)
+      {
+        newNotes.push(note);
+      }
+    }
+    setNotes(newNotes);
+  }
+
+  return (
+    <div className='mydiv'>
+      <Header />
+      <AddNote addNote={handleAddNote}  />
+      <NotesList notes={notes} deleteNote={handleDeleteNote}/>
+    </div>
   );
 }
 
--- src/components/AddNote/AddNote.js
+++ src/components/AddNote/AddNote.js
@@ -2,25 +2,28 @@ import React from 'react';
 import './AddNote.css';
 import { useState } from 'react';
 
-
-let text ="Type to add a note here";
-
-function AddNote( {addNote} ) {
-  let [note,setNote] = useState("")
-  let handleChange = (e)=>{
-    e.preventDefault();
-    setNote(e.target.value);
+function AddNote({ addNote }) {
+  let [content, setContent] = useState("");
+  let handleChange = (e) => {
+    setContent(e.target.value);
   }
 
-  let handleSave = () =>{
-    addNote(note);
+  let handleSave = () => {
+    if (content.length === 0) {
+      alert("Please Insert a valid Note!")
+    }
+    if (content.length > 0) {
+      addNote(content);
+      setContent("");
+    }
   }
+
   return (
     <div>
       <div className='container'>
-        
-        <button className='container-button' onClick={handleSave()} > Save</button>
-        <textarea className='container-textarea' placeholder='Type here to add a note' value={note} onChange={handleChange}> </textarea>
+        <textarea className='container-textarea' placeholder='Type here to add a note' value={content} onChange={handleChange}> </textarea>
+        <button className='container-button' onClick={handleSave} > Save</button>
+
       </div>
     </div>
   )
